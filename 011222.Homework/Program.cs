e444f06547fb552527f1fb6e5257dfd68bd7acf2
Kirill <aklikas@yandex.ru>
2024-07-28 19:33:47 +0300

    projects_one

---
+++ 011222.Homework/Program.cs
@@ -0,0 +1,43 @@
+﻿//на вход функции подается только строка
+//функция возвращает true или false 
+//не переворачивает строку
+
+using System.ComponentModel.Design;
+
+namespace _011222.Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово");
+            string s = Console.ReadLine();           
+            if (CheckString(s))
+            {
+                Console.WriteLine("Палиндром");
+            }
+            else
+            {
+                Console.WriteLine("Не палиндром");
+            }
+        }
+        static bool CheckString(string sCS)
+        {
+            bool result;
+            do
+            {
+                if (sCS[0] == sCS[sCS.Length-1])
+                {
+                    result = true;
+                    sCS = sCS.Substring(1, sCS.Length - 2);
+                }
+                else
+                {
+                    result = false;
+                    break;
+                }
+            } while (sCS.Length>1);
+            return result;
+        }
+    }
+}
